bfdb5b9643a161136d53ab4ce384c8fea56c0ff7
crisskimaryo <crisskimaryo@gmail.com>
2021-12-24 21:44:41 +0300

    add video player & use futurebuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
 import 'package:stackoverflowsamples/youtube_api_v3/yt_api_v3.dart';
 
 void main() => runApp(MyApp());
@@ -18,21 +22,28 @@ class DemoApp extends StatefulWidget {
 }
 
 class _DemoAppState extends State<DemoApp> {
-  static String key = "YOUR-API-KEY";
+  static String key = "YOUR_API_KEY";
 
   YTAPIV3 ytApi = YTAPIV3(key, maxResults: 10);
   List<YT_API> ytResult = [];
-
-  callAPI() async {
-    ytResult = await ytApi.getPlaylistItems("OLAK5uy_miBlW7zfUZGv5U3DDghwHa1yJIpqh8fpw");
-    setState(() {});
-  }
+  YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: '',
+    flags: YoutubePlayerFlags(
+      autoPlay: false,
+      mute: false,
+    ),
+  );
 
   @override
   void initState() {
     super.initState();
-    callAPI();
     print('hello');
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -41,53 +52,75 @@ class _DemoAppState extends State<DemoApp> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Youtube API'),
+        title: Text('Youtube API & Youtube Player'),
+        centerTitle: true,
       ),
       body: Column(
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Text("This example retrieves all the videos in a playlist by id"),
+            child: Text(
+                "This example retrieves all the videos in a playlist by id"),
           ),
           Expanded(
-            child: Container(
-              child: ListView.builder(
-                itemCount: ytResult.length,
-                itemBuilder: (_, int index) => listItem(index),
-              ),
-            ),
+            child: FutureBuilder<List<YT_API>>(
+                future: ytApi
+                    .getPlaylistItems("PLKFGaNXRy6K0cKJTmeHSkr2R1aK8pkJJ3"),
+                builder: (context, AsyncSnapshot<List<YT_API>> snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(
+                        child: Text(
+                      'Error:${snapshot.error}',
+                      style: TextStyle(color: Colors.red),
+                    ));
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+
+                  if (snapshot.hasData) {
+                    snapshot.data.forEach((element) {
+                      ytResult.add(element);
+                    });
+
+                    return Container(
+                      child: ListView.builder(
+                        itemCount: ytResult.length,
+                        itemBuilder: (_, int index) => listItem(index),
+                      ),
+                    );
+                  } else {
+                    return Text('not video data');
+                  }
+                }),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               RaisedButton(
-                  child: Text("<< Prev page",
-                    style: TextStyle(
-                        color: Colors.white
-                    ),
+                  child: Text(
+                    "<< Prev page",
+                    style: TextStyle(color: Colors.white),
                   ),
                   color: Colors.blue,
-                  onPressed: () async{
+                  onPressed: () async {
                     ytResult = await ytApi.prevPage();
-                    if(ytResult != null) {
+                    if (ytResult != null) {
                       setState(() {});
                     }
-                  }
-              ),
+                  }),
               RaisedButton(
-                  child: Text("Next page >>",
-                    style: TextStyle(
-                        color: Colors.white
-                    ),
+                  child: Text(
+                    "Next page >>",
+                    style: TextStyle(color: Colors.white),
                   ),
                   color: Colors.blue,
-                  onPressed: () async{
+                  onPressed: () async {
                     ytResult = await ytApi.nextPage();
-                    if(ytResult != null) {
+                    if (ytResult != null) {
                       setState(() {});
                     }
-                  }
-              ),
+                  }),
             ],
           )
         ],
@@ -96,153 +129,69 @@ class _DemoAppState extends State<DemoApp> {
   }
 
   Widget listItem(index) {
-    return Card(
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 7.0),
-        padding: EdgeInsets.all(12.0),
-        child: Row(
-          children: <Widget>[
-            Image.network(
-              ytResult[index].thumbnail['default']['url'],
-            ),
-            Padding(padding: EdgeInsets.only(right: 20.0)),
-            Expanded(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        ytResult[index].title,
-                        softWrap: true,
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                      Padding(padding: EdgeInsets.only(bottom: 1.5)),
-                      Text(
-                        ytResult[index].channelTitle,
-                        softWrap: true,
-                      ),
-                      Padding(padding: EdgeInsets.only(bottom: 3.0)),
-                      Text(
-                        ytResult[index].url,
-                        softWrap: true,
-                      ),
-                    ]))
-          ],
+    return InkWell(
+      onTap: () {
+        _controller = YoutubePlayerController(
+          flags: YoutubePlayerFlags(
+            autoPlay: true,
+            hideThumbnail: true,
+          ),
+          initialVideoId: ytResult[index].videoId,
+        );
+        showDialog(
+            context: context,
+            builder: (context) {
+              return YoutubePlayer(
+                controller: _controller,
+                showVideoProgressIndicator: true,
+                progressIndicatorColor: Colors.blueAccent,
+                progressColors: ProgressBarColors(
+                  playedColor: Colors.red,
+                  handleColor: Colors.blueAccent,
+                ),
+              );
+            });
+      },
+      child: Card(
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 7.0),
+          padding: EdgeInsets.all(12.0),
+          child: Row(
+            children: <Widget>[
+              Image.network(
+                ytResult[index].thumbnail['default']['url'],
+                fit: BoxFit.cover,
+                width: 100,
+                height: 100,
+              ),
+              Padding(padding: EdgeInsets.only(right: 20.0)),
+              Expanded(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                    Text(
+                      'Channel: ${ytResult[index].channelTitle}',
+                      softWrap: true,
+                    ),
+                    Text(
+                      ytResult[index].title,
+                      maxLines: 2,
+                      style: TextStyle(fontSize: 15.0),
+                    ),
+                    Padding(padding: EdgeInsets.only(bottom: 1.5)),
+                    Padding(padding: EdgeInsets.only(bottom: 3.0)),
+                    Text(
+                      'URL:${ytResult[index].url}',
+                      maxLines: 3,
+                      softWrap: true,
+                      textAlign: TextAlign.left,
+                    ),
+                  ]))
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-/*void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-*/
